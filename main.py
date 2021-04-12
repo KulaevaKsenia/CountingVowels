4389d45c2cf70556c11552f8b0e1287bb64c9132
Kulaeva Ksenia <ksyunel1@gmail.com>
2021-04-13 01:34:13 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+text = input()
+t = str.t(text)
+convert = list(t)
+
+a = convert.count('a')
+e = convert.count('e')
+i = convert.count('i')
+o = convert.count('o')
+u = convert.count('u')
+y = convert.count('y')
+
+print (f"a {a}, e {e}, i {i}, o {o}, u {u}, y {y}")
